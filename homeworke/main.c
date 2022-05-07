7a4b8445895198e50e7c0a418995a7b6ec3ad09f
GoJS9999 <free4787@gmail.com>
2022-05-07 21:18:27 +0900

    Initial Commit

---
+++ homeworke/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  homeworke
+//
+//  Created by 구종석 on 2022/05/07.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
